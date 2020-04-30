189c1d37c0d6a5ce204ecd6849ab7ee42fba99ee
isaac <gilman.isaac@gmail.com>
2020-04-30 09:18:16 -0400

    lab finished

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let scores = [
     {
-        //Jane
         name: "Jane",
         score: 95,
         date: "2020-01-24",
@@ -60,10 +59,11 @@ const deleteScoreByName = (array, name) => {
     array.splice(scoreIndex, 1);
 };
 // local test
-deleteScoreByName(scores, "Joe");
-console.log(scores);
+// deleteScoreByName(scores, "Joe");
+// console.log(scores);
 
 //#5
+//mitch helped 
 const editScore = (array, index, score) => {
     let passed = null;
     if (score >= 60) {
@@ -75,18 +75,21 @@ const editScore = (array, index, score) => {
     array[index].passed = passed; //updates an object in an array
 };
 //local test
-editScore(scores, 1, 55);
-console.log(scores);
+// editScore(scores, 1, 55);
+// console.log(scores);
 
 // #6
+//mitch helped 
 const findScoreByName = (array, name) => {
     return array.find((scoreElement) => {
         return scoreElement.name === name;
     });
 };
-console.log(findScoreByName(scores, "Jane"));
+//local test
+// console.log(findScoreByName(scores, "Jane"));
 
 // #7
+//mitch helped 
 const findLowestScore = (array) => {
     let currentLowestScore = array[0];
     array.forEach(element => {
@@ -96,9 +99,11 @@ const findLowestScore = (array) => {
     });
     return currentLowestScore;
 }
-console.log(findLowestScore(scores));
+//local test
+// console.log(findLowestScore(scores));
 
 //#8
+//mitch helped 
 const findAverageQuizScore = (array) => {
     let currentScores = 0;
     for (let obj of array) {
@@ -106,29 +111,40 @@ const findAverageQuizScore = (array) => {
     }
     return currentScores / array.length;
 };
-console.log(findAverageQuizScore(scores));
+//local test
+// console.log(findAverageQuizScore(scores));
 
 //#9
-//comeback to mitch on
-const filterScores = (array, value) => {
-    return array.filter((obj) => {
-        return obj.name === value;
+//mitch helped 
+const filterScores = (array, property, value) => {
+    return array.filter(obj => {
+        return obj[property] === value;
     });
 };
-console.log(filterScores(scores, "Joe"))
+//local test
+// console.log(filterScores(scores, "name", "Joe"))
 
 //#10
+//mitch helped 
 const sortScoresAsc = (array) => {
-    return array.sort((a, b) => {
-        return a.score < b.score
-    });
-}
-console.log(sortScoresAsc(scores));
+    return array.sort((element1, element2) => {
+        return element1.score < element2.score ? 1 : -1
+    }); ``
+};
+//local test
+// console.log(sortScoresAsc(scores));
+
+//for some reason, when I console log the Dsc (below) and Asc (above) at the same time 
+//both functions restult in descending order 
 
 //#11
+//mitch helped 
 const sortScoresDsc = (array) => {
-    return array.sort((a, b) => {
-        return a.score < b.score
+    return array.sort((element1, element2) => {
+        return element1.score < element2.score ? -1 : 1
     });
-}
-console.log(sortScoresDsc(scores));
+};
+//local test
+// console.log(sortScoresDsc(scores));
+
+
